2cdb5e16a1ee03cf3f71f11a3444ba5a41927a52
achako2012 <a.chako2012@gmail.com>
2020-10-04 19:56:03 +0300

    Updates

---
+++ precache-manifest.be2edce6cc26ca5cf49d571a848c83d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d05282ca6e92cf01bd32a79fb18bc01",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "8b2683d6325660cbe8b6",
+    "url": "/cv/static/css/main.896e688f.chunk.css"
+  },
+  {
+    "revision": "91425beb952d6236a72b",
+    "url": "/cv/static/js/2.ef1e68fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.ef1e68fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2683d6325660cbe8b6",
+    "url": "/cv/static/js/main.723f039e.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  }
+]);
